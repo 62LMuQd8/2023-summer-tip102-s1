51d1dfa723d99cac918e0137c83ba71a19e101e5
62LMuQd8 <>
2023-11-23 22:18:34 +0000

    add u1p1

---
+++ u1p1/index.js
@@ -0,0 +1,17 @@
+// implement requirements
+function containsDuplicate(nums) {
+    const set = new Set();
+    for (const num of nums) {
+        if (set.has(num)) {
+            return true;
+        }
+        set.add(num);
+    }
+    return false;
+}
+
+// testing
+console.log(containsDuplicate([1,2,3,1]));
+console.log(containsDuplicate([1,2,3,4]));
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
+console.log(containsDuplicate([1]));
