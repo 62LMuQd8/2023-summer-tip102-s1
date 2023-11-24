59ee7ab20e06ff63ae3e8233a9718d18f5f50941
62LMuQd8 <>
2023-11-24 02:49:48 +0000

    add u1p5

---
+++ u1p5/graphs.js
@@ -0,0 +1,17 @@
+export class Graphs {
+    // assuming graphs are linear, there are no cycles
+    static destCity(paths) {
+        const set = new Set();
+        // add start cities to a map to O(1) lookups
+        for (const path of paths) {
+            set.add(path[0]);
+        }
+        for (const path of paths) {
+            let dest = path[1];
+            // if end city does not appear as a start city
+            // then we have found the dest city
+            if (!set.has(dest)) return dest;
+        }
+        return "";
+    }
+}
---
+++ u1p5/index.js
@@ -0,0 +1,5 @@
+import { Graphs } from './graphs.js';
+
+console.log(Graphs.destCity([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]));
+console.log(Graphs.destCity([["B","C"],["D","B"],["C","A"]]));
+console.log(Graphs.destCity([["A","Z"]]));
