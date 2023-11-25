b2dee3f240a29ca6a2998ae3e0d661db05a85330
62LMuQd8 <>
2023-11-25 20:57:45 +0000

    add u3p2

---
+++ u3p2/index.js
@@ -0,0 +1,5 @@
+import { NumberService } from './numberService.js';
+
+console.log(NumberService.uniqueOccurrences([1,2,2,1,1,3]));
+console.log(NumberService.uniqueOccurrences([1,2]));
+console.log(NumberService.uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]));
---
+++ u3p2/numberService.js
@@ -0,0 +1,14 @@
+export class NumberService {
+    static uniqueOccurrences(arr) {
+        let map = new Map();
+        // count number of occurrences for each num in arr
+        for (const num of arr) {
+            let occ = map.get(num);
+            map.set(num, occ ? occ + 1 : 1);
+        }
+        // count number of unique occurrences of count
+        let set = new Set(map.values());
+        // compare unique occurrences with number of unique nums in arr
+        return map.size === set.size;
+    }
+}
