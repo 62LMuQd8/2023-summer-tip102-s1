cc4f92bbbed68b07583f81377d9bab0d9a5827c3
62LMuQd8 <>
2023-11-26 04:59:50 +0000

    add u5p5

---
+++ u5p5/index.js
@@ -0,0 +1,5 @@
+import { MathService } from "./mathService.js";
+
+console.log(MathService.isPowerOfFour(16));
+console.log(MathService.isPowerOfFour(5));
+console.log(MathService.isPowerOfFour(1));
---
+++ u5p5/mathService.js
@@ -0,0 +1,13 @@
+export class MathService {
+    // give integer n, determine if n is power of 4
+    // an integer is power of 4, if there exists an integer x such n = 4^x
+    static isPowerOfFour(n) {
+        // 4^x does not have a solution for n = 0
+        if (n === 0) return false;
+        // even though n = 1 is less than 4
+        // there is a solution, 4^0, so n = 1 is a power of four
+        if (n === 1) return true;
+        // power of four statisfies this condition
+        return n % 4 === 0;
+    }
+}
