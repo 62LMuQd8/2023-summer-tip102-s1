8f5fa03c77fd944e91a8424837d3396f2fe593fa
62LMuQd8 <>
2023-12-17 00:38:46 +0000

    add link to CodePath guide

--- u2p2/linkedList.js
+++ u2p2/linkedList.js
@@ -1,3 +1,5 @@
+// read CodePath's explanation: https://guides.codepath.org/compsci/Linked-List-Cycle
+//
 // code template and explanation found here:
 // https://www.geeksforgeeks.org/implementation-linkedlist-javascript/
 import { ListElement } from './listElement.js'
