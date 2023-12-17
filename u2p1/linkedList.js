413598e831ef1037880cc37e8936ec58e0d225ab
62LMuQd8 <>
2023-12-17 00:37:35 +0000

    add link to CodePath guide

--- u2p1/linkedList.js
+++ u2p1/linkedList.js
@@ -1,3 +1,5 @@
+// read CodePath's explanation: https://guides.codepath.org/compsci/Remove-Duplicates-from-Sorted-List
+//
 // code template and explanation found here:
 // https://www.geeksforgeeks.org/implementation-linkedlist-javascript/
 import { ListElement } from './listElement.js'
